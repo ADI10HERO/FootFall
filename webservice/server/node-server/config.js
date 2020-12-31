a35b5c8b264958d3eed41d95d4e86326fa5b4362
Tanvi Shinde <tanvi.shinde46@gmail.com>
2020-12-31 16:46:09 +0530

    Adds multi cam multi tracking of openvino

--- webservice/server/node-server/config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    mqtt: {
-        port: 3001,
-        host: 'localhost',
-        http: {port: 3002, bundle: true, static: './'}  
-    }
-}
--- webservice/server/node-server/server.js
+++
@@ -1,6 +0,0 @@
-const config = require('./config');
-
-/* Start the MQTT server */
-mqtt = require('./mqtt');
-mqtt.configure(config);
-mqtt.start();
