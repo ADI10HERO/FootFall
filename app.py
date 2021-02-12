27a004a6d24a6b906210d85ec48e51a44961fcba
Aditya Srivastava <adityasrivastava301199@gmail.com>
2021-02-12 22:19:34 +0530

    Integrate sockets
    Signed-off-by: Aditya Srivastava <adityasrivastava301199@gmail.com>

--- app.py
+++ app.py
@@ -7,25 +7,24 @@ from flask import (
     send_from_directory,
 )
 from flask_socketio import SocketIO, emit
-import logging
 import time
-from logging import info
 from main import main
 from threading import Thread, Event
+from utils.misc import encode
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 app.config['DEBUG'] = True
 
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode=None, logger=True, engineio_logger=True)
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode=None, logger=False, engineio_logger=False)
 
 
 input = None
 frames = []
 frame_counts = []
 total_count = 0
-logging.basicConfig(level=logging.INFO)
+num_cams = 0
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -37,61 +36,33 @@ def home():
         return render_template("index.html")
 
 
-@app.route("/basic", methods=["GET", "POST"])
-def basic():
+@app.route("/info/1", methods=["GET", "POST"])
+def get_num_cams():
+    global input, num_cams
     if request.method == "POST":
-        num_cams = request.form.get("num_cams")
+        num_cams = int(request.form.get("num_cams"))
         ans = request.form.get("ques")
+
         if ans == "yes":
-            total_count, cam_counts, frames = main(
-                {"num_cams": num_cams, "ondevice": True}
-            )
-            # TODO: Create mtc object here or call driver code.
-            return render_template("view_stream.html")
-        return render_template("get_cam_info.html", num_cams=int(num_cams))
+            input = [i for i in range(num_cams)]
+            return render_template("dashboard.html", num_cams=num_cams)
+        return render_template("get_cam_info.html", num_cams=num_cams)
 
     if request.method == "GET":
         return render_template("index.html")
 
 
-@app.route("/video_stream", methods=["POST"])
-def process():
-    global input, frames, frame_counts
+@app.route("/info/2", methods=["POST"])
+def get_input_urls():
+    global input
     if request.method == "POST":
         data = dict(request.form)
-        data["ondevice"] = False
-
-        num_cams = int(data["num_cam"])
         input = [data["cam_" + str(i+1)] for i in range(num_cams)]
-
-        return render_template("dashboard.html",
-            cam_counts=frame_counts,
-            num_cam=num_cams
-        )
+        return render_template("dashboard.html", num_cams=num_cams)
 
     if request.method == "GET":
         return render_template("dashboard.html")
 
-
-def view_stream(id):
-
-    global input, frames, frame_counts, total_count
-    if input is None or frames == [] or frame_counts == []:
-        time.sleep(1)
-
-    for data in main(input):
-        frames = [ frame[0] for frame in data ]
-        frame_counts = [ frame[1] for frame in data ]
-        total_count = sum(frame_counts)
-        print(total_count, frame_counts)
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frames[id-1].tobytes() + b'\r\n\r\n')
-
-
-@app.route("/view/<idx>")
-def get_single_frame(idx):
-    idx = int(idx)
-    return Response(view_stream(idx), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 # socket routes below
 thread = Thread()
@@ -100,16 +71,26 @@ thread_stop_event = Event()
 
 def getupdatedinfo():
 
-    global total_count, frame_counts
+    global total_count, frame_counts, frames, input
 
+    callback = main(input)
     print("Starting stream")
     while not thread_stop_event.isSet():
+        t1 = time.time()
+        print("Takes {} time to get 1 batch of frames".format(time.time()-t1))
+        resp = next(callback)
+        frames = [ encode(frame[0]) for frame in resp ]
+        frame_counts = [ frame[1] for frame in resp ]
+        total_count = sum(frame_counts)
         data = {
             'total_count': total_count,
-            'frame_counts': [2, 3]
+            'frame_counts': frame_counts,
+            'frames': frames
         }
+
         socketio.emit('newdata', data, namespace='/test')
-        socketio.sleep(2)
+        time.sleep(1)
+        # socketio.sleep(2)
 
 
 @socketio.on('connect', namespace='/test')
